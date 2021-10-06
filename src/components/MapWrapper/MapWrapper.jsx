bb1757636400b672d248e32b4c617e4bb2ad8bb9
Molasz <martiolasz@gmail.com>
2021-10-07 01:08:57 +0200

    hide api key

--- src/components/MapWrapper/MapWrapper.jsx
+++ src/components/MapWrapper/MapWrapper.jsx
@@ -1,19 +1,16 @@
 import React from "react";
 import { Wrapper } from "@googlemaps/react-wrapper";
 
-import Map from "../Map/Map"
+import Map from "../Map/Map";
 
-const key = "AIzaSyAK8uXlfZrzUSEWe5eWWhE0ykXa-0ic6Z8"
+import { key } from "../../config/MAPS_KEY";
 
 function MapWrapper() {
-  const mapRender = (status) => status === "SUCCESS" ? null : <p> {status} </p>
+  const mapRender = (status) =>
+    status === "SUCCESS" ? null : <p> {status} </p>;
 
   return (
-    <Wrapper
-      apiKey={key}
-      libraries={['places']}
-      render={mapRender}
-    >
+    <Wrapper apiKey={key} libraries={["places"]} render={mapRender}>
       <Map />
     </Wrapper>
   );
